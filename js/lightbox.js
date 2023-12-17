13292a8bf88049486620e5611c6bd993ce7dc929
PsychicNull <wfranz.lee@gmail.com>
2023-12-16 22:26:42 -0500

    Update lightbox.js

--- js/lightbox.js
+++ js/lightbox.js
@@ -3,42 +3,74 @@
 /* lightbox.js for Zach Feldman */
 
 $(document).ready(function() {
+    // Function to detect touch devices
+    function isTouchDevice() {
+        return 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+    }
+
     // Open lightbox
-    $('.lightbox-trigger').click(function() {
-        var src = $(this).attr('src');
-        $('#lightbox-image').attr('src', src);
-        $('#lightbox').fadeIn();
-        $('body').css('overflow', 'hidden'); // Prevent scrolling
-    });
-
-    // Close lightbox
-    $('#close-lightbox').click(function() {
-        $('#lightbox').fadeOut();
-        $('body').css('overflow', ''); // Enable scrolling again
-    });
-
-    // Close lightbox when clicking anywhere in the lightbox
-    $('#lightbox').click(function() {
-        $(this).fadeOut();
-        $('body').css('overflow', ''); // Enable scrolling again
-    });
-
-
-    // Mobile touch
     $('.lightbox-trigger').on('click touchstart', function(e) {
-        e.preventDefault();  // Prevent default action (like navigating to the image source)
+        e.preventDefault();
         var src = $(this).attr('src');
         $('#lightbox-image').attr('src', src);
         $('#lightbox').fadeIn();
-        $('body').css('overflow', 'hidden'); // Prevent scrolling
+        $('body').css('overflow', 'hidden');
     });
 
-    // Close lightbox
+    // Close lightbox when 'X' is clicked/tapped
     $('#close-lightbox').on('click touchstart', function(e) {
         e.preventDefault();
         $('#lightbox').fadeOut();
-        $('body').css('overflow', ''); // Enable scrolling again
+        $('body').css('overflow', '');
     });
+
+    // If not a touch device, allow closing lightbox by clicking anywhere
+    if (!isTouchDevice()) {
+        $('#lightbox').on('click', function(e) {
+            if (e.target.id !== 'lightbox-image') {
+                $(this).fadeOut();
+                $('body').css('overflow', '');
+            }
+        });
+    }
+
+
+    // // Open lightbox
+    // $('.lightbox-trigger').click(function() {
+    //     var src = $(this).attr('src');
+    //     $('#lightbox-image').attr('src', src);
+    //     $('#lightbox').fadeIn();
+    //     $('body').css('overflow', 'hidden'); // Prevent scrolling
+    // });
+
+    // // Close lightbox
+    // $('#close-lightbox').click(function() {
+    //     $('#lightbox').fadeOut();
+    //     $('body').css('overflow', ''); // Enable scrolling again
+    // });
+
+    // // Close lightbox when clicking anywhere in the lightbox
+    // $('#lightbox').click(function() {
+    //     $(this).fadeOut();
+    //     $('body').css('overflow', ''); // Enable scrolling again
+    // });
+
+
+    // // Mobile touch
+    // $('.lightbox-trigger').on('click touchstart', function(e) {
+    //     e.preventDefault();  // Prevent default action (like navigating to the image source)
+    //     var src = $(this).attr('src');
+    //     $('#lightbox-image').attr('src', src);
+    //     $('#lightbox').fadeIn();
+    //     $('body').css('overflow', 'hidden'); // Prevent scrolling
+    // });
+
+    // // Close lightbox
+    // $('#close-lightbox').on('click touchstart', function(e) {
+    //     e.preventDefault();
+    //     $('#lightbox').fadeOut();
+    //     $('body').css('overflow', ''); // Enable scrolling again
+    // });
 
     // Close lightbox when clicking/tapping anywhere in the lightbox
     // $('#lightbox').on('click touchstart', function(e) {
