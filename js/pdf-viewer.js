b7ee65db0836d2a6b43b7b5e422fc1aa128eab5f
PsychicNull <wfranz.lee@gmail.com>
2023-12-29 12:39:33 -0500

    Changed PDF viewer

--- js/pdf-viewer.js
+++ js/pdf-viewer.js
@@ -1,12 +1,28 @@
-function adjustPDFZoom() {
-    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    var pdfEmbed = document.getElementById('pdfEmbed');
-    var zoomLevel = width < 1080 ? 60 : 90; // 50% zoom for screens smaller than 1080px
-    pdfEmbed.src = "../pdfs/clocking-out-catalog.pdf#zoom=" + zoomLevel;
-}
+/*eslint-env es6*/
 
-// Adjust zoom when the page loads
-adjustPDFZoom();
+/* pdf-viewer.js for Zachary B. Feldman */
 
-// Adjust zoom on window resize
-window.onresize = adjustPDFZoom;
+$(document).ready(function() { 
+    function adjustPDFZoom() {
+        var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        var pdfEmbed = document.getElementById('pdfEmbed');
+        
+        // Determine the zoom level based on window width
+        var zoomLevel;
+        if (width <= 768) {
+            zoomLevel = 20; // 30% zoom for screens 768px or smaller
+        } else if (width < 1080) {
+            zoomLevel = 60; // 60% zoom for screens smaller than 1080px but larger than 768px
+        } else {
+            zoomLevel = 90; // 90% zoom for larger screens
+        }
+
+        pdfEmbed.src = "../pdfs/clocking-out-catalog.pdf#zoom=" + zoomLevel;
+    }
+
+    // Adjust zoom when the page loads
+    adjustPDFZoom();
+
+    // Adjust zoom on window resize
+    window.onresize = adjustPDFZoom;
+});
